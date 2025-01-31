87bc75ff8616a3201bdd5eca0abb1cd92b99c29f
AlexMan123456 <alex.man03888@gmail.com>
2025-01-31 15:53:09 +0000

    Completed CostCalculator class

---
+++ src/objectOrientedProgrammingPartOne/CostCalculator.java
@@ -0,0 +1,15 @@
+package objectOrientedProgrammingPartOne;
+
+public class CostCalculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public CostCalculator(Floor floor, Carpet carpet){
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea()*carpet.getCost();
+    }
+}
